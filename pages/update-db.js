9fd17e470106f21a4c92d95982723ddde8ea3fc1
iskanderomanov <iskanderomanov@gmail.com>
2023-07-13 03:52:45 +0600

    update service

--- pages/update-db.js
+++ pages/update-db.js
@@ -1,9 +1,9 @@
-import {useState} from 'react';
-import {useRouter} from 'next/router';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 import SelectWithSearch from '../components/SelectWithSearch';
 import FormInput from '../components/FormInput';
-import {parseCookies} from "nookies";
-import Navigation from "../components/Navigation";
+import { parseCookies } from 'nookies';
+import Navigation from '../components/Navigation';
 
 export default function UpdateDBPage() {
     const [formData, setFormData] = useState({
@@ -15,11 +15,26 @@ export default function UpdateDBPage() {
     const router = useRouter();
 
     const handleInputChange = (event) => {
-        const {name, value} = event.target;
-        setFormData((prevFormData) => ({
-            ...prevFormData,
-            [name]: value,
-        }));
+        const { name, value, type, files } = event.target;
+
+        if (type === 'file' && files.length) {
+            const file = files[0];
+            const reader = new FileReader();
+
+            reader.onloadend = () => {
+                setFormData((prevFormData) => ({
+                    ...prevFormData,
+                    [name]: reader.result,
+                }));
+            };
+
+            reader.readAsDataURL(file);
+        } else {
+            setFormData((prevFormData) => ({
+                ...prevFormData,
+                [name]: value,
+            }));
+        }
     };
 
     const handleSubmit = async (event) => {
@@ -51,22 +66,26 @@ export default function UpdateDBPage() {
         }
     };
 
-
     return (
         <div>
             <div>
-            <Navigation></Navigation>
+                <Navigation></Navigation>
             </div>
             <div className="container">
                 <h1>Страница обновления БД</h1>
                 <form onSubmit={handleSubmit}>
                     <div className="form-group">
                         <label htmlFor="service_id">Сервис</label>
-                        <SelectWithSearch apiUrl={`${process.env.NEXT_PUBLIC_GET_LIST_SERVICES_URL}`} required
-                                          name="serviceId"
-                                          onSelectChange={(selectedValue) =>
-                                              handleInputChange({target: {name: 'serviceId', value: selectedValue}})
-                                          }/>
+                        <SelectWithSearch
+                            apiUrl={`${process.env.NEXT_PUBLIC_GET_LIST_SERVICES_URL}`}
+                            required
+                            name="serviceId"
+                            onSelectChange={(selectedValue) =>
+                                handleInputChange({
+                                    target: { name: 'serviceId', value: selectedValue },
+                                })
+                            }
+                        />
                     </div>
                     <div className="form-group">
                         <label htmlFor="identifierOrder">Идентификатор*</label>
@@ -98,7 +117,6 @@ export default function UpdateDBPage() {
                             type="file"
                             id="file"
                             name="file"
-                            value={formData.file}
                             accept=".xls,.xlsx,.dbf,.txt,.csv"
                             onChange={handleInputChange}
                             required
@@ -109,56 +127,55 @@ export default function UpdateDBPage() {
                 </form>
 
                 <style jsx>{`
-                  .container {
-                    max-width: 500px;
-                    margin: 10px auto 0;
-                    padding: 2rem;
-                    border: 1px solid #ccc;
-                    border-radius: 5px;
-                  }
+          .container {
+            max-width: 500px;
+            margin: 10px auto 0;
+            padding: 2rem;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+          }
 
-                  h1 {
-                    text-align: center;
-                    margin-bottom: 2rem;
-                    font-family: sans-serif;
-                    font-size: 1.5rem;
-                  }
+          h1 {
+            text-align: center;
+            margin-bottom: 2rem;
+            font-family: sans-serif;
+            font-size: 1.5rem;
+          }
 
-                  .form-group {
-                    margin-bottom: 2rem;
-                  }
+          .form-group {
+            margin-bottom: 2rem;
+          }
 
-                  label {
-                    display: block;
-                    margin-bottom: 0.5rem;
-                    font-weight: bold;
-                    font-family: sans-serif;
-                  }
+          label {
+            display: block;
+            margin-bottom: 0.5rem;
+            font-weight: bold;
+            font-family: sans-serif;
+          }
 
-                  .input-field {
-                    width: 100%;
-                    padding: 0.5rem;
-                    border-radius: 4px;
-                    border: 1px solid #ccc;
-                    font-family: sans-serif;
-                  }
+          .input-field {
+            width: 100%;
+            padding: 0.5rem;
+            border-radius: 4px;
+            border: 1px solid #ccc;
+            font-family: sans-serif;
+          }
 
-                  button[type='submit'] {
-                    background-color: grey;
-                    color: #fff;
-                    border: none;
-                    padding: 0.5rem 1rem;
-                    border-radius: 4px;
-                    cursor: pointer;
-                    width: 100%;
-                  }
+          button[type='submit'] {
+            background-color: grey;
+            color: #fff;
+            border: none;
+            padding: 0.5rem 1rem;
+            border-radius: 4px;
+            cursor: pointer;
+            width: 100%;
+          }
 
-                  button[type='submit']:hover {
-                    background-color: #0069d9;
-                  }
-                `}</style>
+          button[type='submit']:hover {
+            background-color: #0069d9;
+          }
+        `}</style>
             </div>
         </div>
-
     );
 }
