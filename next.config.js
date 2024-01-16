876f53be901e68f69cdb878d407f982b26c9c66c
TiBolTi <nikita.kopyltsov@mail.ru>
2024-01-16 16:17:21 +0600

    fix bug

--- next.config.js
+++ next.config.js
@@ -1,22 +1,7 @@
 /** @type {import('next').NextConfig} */
-const {nextui} = require("@nextui-org/react");
 require('dotenv').config();
-const nextConfig = {
-    content: [
-        // ...
-        "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
-    ],
-    theme: {
-        extend: {},
-    },
-    darkMode: "class",
-    plugins: [nextui()]
-}
-
-module.exports = {
-    // Другие конфигурации...
-    plugins: [nextui()],
-};
+const nextConfig = {}
+module.exports = nextConfig
 // next.config.js
 // module.exports = {
 //     async redirects() {
