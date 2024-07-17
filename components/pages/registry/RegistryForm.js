4c67e093fada72b420442dc84f363353228633c3
TiBolTi <nikita.kopyltsov@mail.ru>
2024-07-17 11:37:18 +0600

    add totalpay field name

--- components/pages/registry/RegistryForm.js
+++ components/pages/registry/RegistryForm.js
@@ -698,7 +698,7 @@ const RegistryForm = ({oldFormData = [], formData = [], onDataFieldsChange}) => 
                                 className="input-field"
                                 id="totalpayFieldName"
                                 name="totalpayFieldName"
-                                defaultValue={formData.additional_fields[0].totalpayFieldName ?? 'ИТОГО:'}
+                                defaultValue={formData.additional_fields[0].totalpayFieldName}
                                 onChange={handleAdditionalFieldChange}
                                 required
                             />
